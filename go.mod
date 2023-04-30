119733e32d38e9ff316a6eed1e67f7d5153e6d11
Nick Ater <30247698+nickater@users.noreply.github.com>
2023-04-30 00:28:25 -0400

    init

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/nickater/hackrchat-cli
+
+go 1.20
---
+++ main.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"fmt"
+)
+
+func init() {
+	// Init
+	fmt.Println("Init")
+}
+
+func main() {
+	// Hello world
+	fmt.Println("Hello world")
+}
