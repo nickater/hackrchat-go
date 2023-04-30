1d4048650fe33891dfac89e5704f629629962b3f
Nick Ater <30247698+nickater@users.noreply.github.com>
2023-04-30 13:35:14 -0400

    added auth service

--- service/auth.go
+++ service/auth.go
@@ -1,1 +1,33 @@
 package service
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+
+	"github.com/nickater/hackrchat-cli/model"
+)
+
+func LoginUser(credentials model.Credential) (string, error) {
+	fmt.Println(credentials.Username)
+	result, err := http.Get("https://catfact.ninja/fact")
+
+	if err != nil {
+		fmt.Println(err)
+	}
+	defer result.Body.Close()
+	var catFactResult struct {
+		Fact   string `json: "#fact"`
+		Length int    `json: "#length"`
+	}
+
+	json.NewDecoder(result.Body).Decode(&catFactResult)
+	//Convert the body to type string
+	sb := fmt.Sprintf("Fact: %s, Length: %d", catFactResult.Fact, catFactResult.Length)
+
+	return sb, nil
+}
+
+func RegisterUser() {
+	fmt.Println("RegisterUser")
+}
